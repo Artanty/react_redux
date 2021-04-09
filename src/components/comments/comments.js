91a2f5c2e85d1c2fb57ecaf1979bd8fe12649983
Artanty <thomasway7@gmail.com>
2021-04-09 22:54:36 +0300

    refactor cards with loop via map

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -3,20 +3,24 @@ import CommentDetail from './commentDetail';
 import faker from 'faker';
 import ApprovalCard from "./approvalCard";
 
+
 function Comments() {
     return (
         <div className="ui container comments">
             <ApprovalCard>Just text</ApprovalCard>
             <ApprovalCard><h4>Warning!</h4>Just text</ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="lol1" timeAgo="Todat at 4:05PM" content="cool" avatar={faker.image.avatar()}/>
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="lol2" timeAgo="Todat at 7:35PM" content="cool" avatar={faker.image.avatar()}/>
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="lol3" timeAgo="Todat at 6:15PM" content="cool" avatar={faker.image.avatar()}/>
-            </ApprovalCard>
+
+            {[...Array(3)].map((x, i) =>
+
+                <ApprovalCard>
+                    <CommentDetail
+                        author={faker.lorem.words(1)}
+                        timeAgo={faker.date.weekday()}
+                        content={faker.lorem.words(5)}
+                        avatar={faker.image.avatar()}/>
+                </ApprovalCard>
+            )}
+
         </div>
     );
 }
