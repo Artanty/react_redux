391a6c52037442d033b53590b36f5488851ca0b1
Artanty <thomasway7@gmail.com>
2021-04-09 20:52:03 +0300

    configure faker

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,11 +1,12 @@
 import React from "react";
+import faker from 'faker';
 
 function Comments() {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
