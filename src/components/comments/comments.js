85694db864477b6ad553fdb035509e45d23085bf
Artanty <thomasway7@gmail.com>
2021-04-09 22:01:25 +0300

    passing component to child via props

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,11 +1,15 @@
 import React from "react";
 import CommentDetail from './commentDetail';
 import faker from 'faker';
+import ApprovalCard from "./approvalCard";
 
 function Comments() {
     return (
         <div className="ui container comments">
-            <CommentDetail author="lol1" timeAgo="Todat at 4:05PM" content="cool" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="lol1" timeAgo="Todat at 4:05PM" content="cool" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
             <CommentDetail author="lol2" timeAgo="Todat at 7:35PM" content="cool" avatar={faker.image.avatar()}/>
             <CommentDetail author="lol3" timeAgo="Todat at 6:15PM" content="cool" avatar={faker.image.avatar()}/>
         </div>
