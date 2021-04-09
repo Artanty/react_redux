75c910af3f38fd9767e30df0b21f0004528999a6
Artanty <thomasway7@gmail.com>
2021-04-09 22:04:04 +0300

    passing different types via props

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -6,12 +6,17 @@ import ApprovalCard from "./approvalCard";
 function Comments() {
     return (
         <div className="ui container comments">
+            <ApprovalCard>Just text</ApprovalCard>
+            <ApprovalCard><h4>Warning!</h4>Just text</ApprovalCard>
             <ApprovalCard>
                 <CommentDetail author="lol1" timeAgo="Todat at 4:05PM" content="cool" avatar={faker.image.avatar()}/>
             </ApprovalCard>
-
-            <CommentDetail author="lol2" timeAgo="Todat at 7:35PM" content="cool" avatar={faker.image.avatar()}/>
-            <CommentDetail author="lol3" timeAgo="Todat at 6:15PM" content="cool" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="lol2" timeAgo="Todat at 7:35PM" content="cool" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="lol3" timeAgo="Todat at 6:15PM" content="cool" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 }
