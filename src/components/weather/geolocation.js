68e3da27faf845bcf590d5077c566a4bbbc8bc26
Artanty <thomasway7@gmail.com>
2021-04-09 23:15:37 +0300

    refactor from functional to class comp

--- src/components/weather/geolocation.js
+++ src/components/weather/geolocation.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ApprovalCard from "../comments/approvalCard";
 
 
-const Geolocation = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-    return <div>Hi there!</div>;
+class Geolocation extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
 }
 
 export default Geolocation;
