34e9cb86eb961b3da672642901a084fe7367b116
Artanty <thomasway7@gmail.com>
2021-04-10 00:36:37 +0300

    + setState

--- src/components/weather/geolocation.js
+++ src/components/weather/geolocation.js
@@ -3,12 +3,21 @@ import ApprovalCard from "../comments/approvalCard";
 
 
 class Geolocation extends React.Component {
-    render(){
+    constructor(props){
+        super(props);
+
+        this.state = { lat: null};
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({lat: position.coords.latitude})
+            },
             err => console.log(err)
         );
-        return <div>Latitude: </div>;
+    }
+    render(){
+
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
