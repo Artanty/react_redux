24f8d3c5dd6313a6eb645b2e169b5bd0940473ba
Artyom <thomasway7@gmail.com>
2021-04-09 16:25:25 +0300

    start translate project

---
+++ src/components/languages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Languages extends React.Component {
+
+  render() {
+
+    return (
+      <div className="ui field">
+        <select>
+            <option>1</option>
+            <option>2</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default  Languages;
---
+++ src/components/translate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+class Translate extends React.Component {
+    render() {
+        return (
+            <div>myau</div>
+        );
+    }
+}
+
+export default Translate;
